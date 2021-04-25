fd5354c29ec3d3ddc146b094672a580d179cd1ea
Karl Rapur <rapurkarl@gmail.com>
2021-04-25 19:09:01 +0300

    Added PrimeVue library-

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
   <router-view />
 </template>
 
+<script lang="ts">
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css";
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
