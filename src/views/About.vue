4f279f59b7ac164e0951e193314f02e67efe442e
Karl Rapur <rapurkarl@gmail.com>
2021-05-01 20:51:11 +0300

    Added About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template class="p-grid">
+  <Card class="p-col-12 p-md-4 p-shadow-14 p-m-auto">
+    <template #content>
+      <h1>CGI internship task</h1>
+      <h2>
+        <br />Karl Rapur <br />
+        karl.rapur@ut.ee
+      </h2>
+    </template>
+  </Card>
 </template>
+
+<script lang="ts">
+import Card from "primevue/card";
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style></style>
